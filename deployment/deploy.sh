d903e71aec22c54a48fa742ae0596f2c5bd4a821
acrostm <zjc971109@gmail.com>
2023-09-27 17:22:02 -0700

    refine shell file

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -13,24 +13,55 @@ if [ "$CURRENT_DIR" != "$EXPECTED_DIR" ]; then
   exit 1
 fi
 
-# 执行 git pull
-git pull origin master
+# 检查当前Shell类型
+CURRENT_SHELL="$SHELL"
 
-# 进入前端目录
-cd "$FRONTEND_HOME" || exit
+if [ "$CURRENT_SHELL" = "/bin/bash" ]; then
+  echo "当前Shell是Bash:"
+  # 在Bash中执行操作
+  # 示例操作：输出Bash特定的信息
 
-# 安装前端依赖并构建
-npm install
-npm run build
-serve -s build
+  # 执行 git pull
+  git pull origin master
 
-# 进入后端目录
-cd "$BACKEND_HOME" || exit
+  # 进入前端目录
+  cd "$FRONTEND_HOME" || exit
+  npm install
+  npm run build
 
-# 安装后端依赖并启动
-npm install
-npm start
+  # 进入后端目录
+  cd "$BACKEND_HOME" || exit
+  npm install
 
+  # 分别进入 screen 会话并启动服务
+  screen -dmS frontend bash -c "npm start; exec bash"
+  screen -dmS backend bash -c "npm start; exec bash"
 
+elif [ "$CURRENT_SHELL" = "/bin/zsh" ]; then
+  echo "当前Shell是Zsh:"
+  # 在Zsh中执行操作
+  # 示例操作：输出Zsh特定的信息
 
+  # 执行 git pull
+  git pull origin master
 
+  # 进入前端目录
+  cd "$FRONTEND_HOME" || exit
+  npm install
+  npm run build
+
+  # 进入后端目录
+  cd "$BACKEND_HOME" || exit
+  npm install
+
+  # 分别进入 screen 会话并启动服务
+  screen -dmS frontend bash -c "npm start; exec bash"
+  screen -dmS backend bash -c "npm start; exec bash"
+
+else
+  echo "未知Shell: $CURRENT_SHELL"
+fi
+
+# 共同的操作
+# 示例操作：输出通用信息
+echo "通用操作:"
