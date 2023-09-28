488404009f653c1d3daed6bd59aebae88f162292
acrostm <zjc971109@gmail.com>
2023-09-28 01:14:48 -0700

    fix shell: refine the shell

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,26 +4,28 @@
 FRONTEND_HOME="/home/jiachzha/github/project101/frontend"
 BACKEND_HOME="/home/jiachzha/github/project101/backend"
 
-# 检查当前Shell类型
-CURRENT_SHELL="$SHELL"
-
 if [ "$CURRENT_SHELL" = "/bin/bash" ]; then
   echo "当前Shell是Bash:"
   # 在Bash中执行操作
   # 示例操作：输出Bash特定的信息
 
   # 执行 git pull
+  echo "> Running git pull [responsitory: acrostm/project101, branch: master] ------------------"
   git pull origin master
 
   # 进入前端目录
   cd "$FRONTEND_HOME" || exit
+  echo "> Entered $(PWD) /\ Running npm install -------------------------------------------------"
   npm install
+  echo "> Running npm run build -----------------------------------------------------------------"
   npm run build
+  echo "> cleaning detached screen --------------------------------------------------------------"
   screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs -I {} screen -X -S {} quit
   screen -dmS frontend serve -S build
 
   # 进入后端目录
   cd "$BACKEND_HOME" || exit
+  echo "> Entered $(PWD) /\ Running npm install -------------------------------------------------"
   npm install
   screen -dmS backend npm start
 
@@ -34,20 +36,27 @@ elif [ "$CURRENT_SHELL" = "/usr/bin/zsh" ]; then
   # 示例操作：输出Zsh特定的信息
 
   # 执行 git pull
+  echo "> Running git pull [responsitory: acrostm/project101, branch: master] ------------------"
   git pull origin master
 
   # 进入前端目录
   cd "$FRONTEND_HOME" || exit
+  echo "> Entered $(PWD) /\ Running npm install -------------------------------------------------"
   npm install
+  echo "> Running npm run build -----------------------------------------------------------------"
   npm run build
+  echo "> cleaning detached screen --------------------------------------------------------------"
   screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs -I {} screen -X -S {} quit
   screen -dmS frontend serve -S build
 
   # 进入后端目录
   cd "$BACKEND_HOME" || exit
+  echo "> Entered $(PWD) /\ Running npm install -------------------------------------------------"
   npm install
   screen -dmS backend npm start
 
 else
   echo "未知Shell: $CURRENT_SHELL"
 fi
+
+echo "> React and Express service started /\ turn to https:www.jiachzha.top to check it out!"
